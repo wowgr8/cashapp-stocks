c9e22b619f6fb81a859315dee01a827bb704880c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-11-29 22:11:03 -0800

    add StocksApi import statement and component tag.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import StocksApi from './StocksApi';
 
 function App() {
   return (
     <div className="App">
+      <StocksApi />
     </div>
   );
 }

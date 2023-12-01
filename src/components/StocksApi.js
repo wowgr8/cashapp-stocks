932a53fffb123818650cb19e669eea05286e7302
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-11-30 23:15:51 -0800

    add GET request - WIP, useState and useEffect hooks to call and set data.

--- src/components/StocksApi.js
+++ src/components/StocksApi.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function StocksApi() {
+  const [stockData, setStockData] = useState();
+
+  useEffect(()=> {
+    getStockData();
+  }, [])
+
+  const getStockData = async () =>{
+    const data = await axios.get("https://storage.googleapis.com/cash-homework/cash-stocks-api/portfolio.json");
+    setStockData(data);
+    console.log(data)
+    console.log("data")
+  };
+
   return (
-    <div>StocksApi</div>
+    <>
+      <div>StocksApi</div>
+    </>
   )
 }
 
